0b503b04b7b541c492ebc0fe7a44734d8255c291
payalacharya <payalaacharya00@gmail.com>
2026-03-14 12:11:20 +0530

    This is my first commit

---
+++ Script1.sh
@@ -0,0 +1,5 @@
+echo "We are trying this practical from linux EC2"
+echo "We are from batch-25 and learning git/gihub"
+echo "We are learning Branching"
+echo "I am learning Git"
+echo "We are leaning Devops"
